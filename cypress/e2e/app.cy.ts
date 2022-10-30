f22759798aaeab675c530927721252b9a46b8440
Brisa Díaz <brisaabigaildiaz2000@gmail.com>
2022-10-29 23:25:51 -0300

    test: add dark mode integration test (#8)
    * test: add dark mode integration test
    
    * fix: add NODE_ENV to testing
    
    * fix: mocke thefault theme before load

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,39 @@
-describe('Navigation', () => {
-  it('should navigate to the home page', () => {
+describe('Theme', () => {
+  it('should toggle dark mode', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
+
+    const startTheme = 'dark'
+
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        localStorage.setItem('theme', startTheme)
+        cy.stub(win, 'matchMedia')
+          .withArgs(`(prefers-color-scheme: ${startTheme})`)
+          .returns({
+            matches: true
+          })
+      }
+    })
+
+    const toChangeTheme1 = startTheme === 'dark' ? 'light' : 'dark'
+
+    cy.get('[data-testid="theme switch"]')
+      .as('themeSwitch')
+      .invoke('attr', 'aria-label')
+      .should('contain', `switch to ${toChangeTheme1} mode`)
+
+    const toChangeTheme2 = startTheme
+
+    cy.get('@themeSwitch').click()
+
+    // set the theme mode to component
+    cy.get('[data-testid="theme switch"]')
+      .as('themeSwitch')
+      .invoke('attr', 'data-theme')
+      .should('contain', toChangeTheme1)
+
+    cy.get('@themeSwitch')
+      .invoke('attr', 'aria-label')
+      .should('contain', `switch to ${toChangeTheme2} mode`)
   })
 })
