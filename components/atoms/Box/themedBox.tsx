d52c46dc816016c948e8f035ab56b2b90d22b5d1
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-11-18 18:48:31 -0300

    refactor: rename themed button file

--- components/atoms/Box/themedBox.tsx
+++
@@ -1,9 +0,0 @@
-import { forwardRef, Ref } from 'react'
-import { useTheme } from '@common/providers/ThemeProvider'
-import Box, { BoxProps } from './index'
-function ThemedBox(props: BoxProps, ref?: Ref<any>) {
-  const { mode } = useTheme()
-
-  return <Box themeMode={mode} {...props} ref={ref} />
-}
-export default forwardRef(ThemedBox)
