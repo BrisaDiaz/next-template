a6883231cd724d314de2f202a8dc90c578c5d045
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-11-20 22:49:16 -0300

    feat: add combineCustomStyles function

--- common/utils/createStyles.test.tsx
+++ common/utils/createStyles.test.tsx
@@ -1,4 +1,5 @@
-import { createStyle } from './createStyles'
+import { createStyle, combineCustomStyles } from './createStyles'
+import { theme, CustomStyle } from '@common/utils'
 import { breakpoints } from './themeSchemas'
 const className = 'jadlkadgj'
 test('should generate correct styles when pass a single object', () => {
@@ -65,4 +66,48 @@ test('should generate correct styles when pass a custom breakpoint', () => {
   expect(styles).toContain(`@media (min-width: 500px)`)
   expect(styles).toContain(`.title`)
   expect(styles).toContain(`font-size:1.25rem;`)
+})
+test(' combineCustomStyles() combines props in a single object when selector is found repeated', () => {
+  const oldStyles = {
+    selector: '.box',
+    css: {
+      mt: theme.space['4'],
+      maxW: theme.size['container-md'],
+      w: theme.size.full,
+      minH: theme.size['72'],
+      fontWeight: theme.fontWeight.bold,
+      bgColor: theme.paletteColor['red-300'],
+      pl: { sm: '45px', 545: '15px' }
+    }
+  }
+  const newStyles = [
+    {
+      selector: '.box',
+      css: {
+        pl: theme.space['2'],
+        bgColor: theme.paletteColor['pink-300']
+      }
+    },
+    {
+      selector: '.box:hover',
+      css: {
+        bgColor: theme.paletteColor['green-300']
+      }
+    }
+  ]
+  const expectCombined = {
+    selector: '.box',
+    css: {
+      bgColor: 'var(--colors-pink-300)',
+      fontWeight: 'var(--fontWeights-bold)',
+      maxW: 'var(--sizes-container-md)',
+      minH: 'var(--sizes-72)',
+      mt: 'var(--space-4)',
+      pl: 'var(--space-2)',
+      w: 'var(--sizes-full)'
+    }
+  }
+  const result = combineCustomStyles(oldStyles, newStyles) as CustomStyle[]
+  expect(result).toHaveLength(2)
+  expect(result[0]).toEqual(expectCombined)
 })
