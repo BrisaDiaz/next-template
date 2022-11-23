638e8dab30349c67ee6e4aceae917c98f6ed95c3
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-11-23 16:12:46 -0300

    feat: add Box component variants

--- components/atoms/Box/Themed.tsx
+++ components/atoms/Box/Themed.tsx
@@ -1,9 +1,45 @@
 import { forwardRef, Ref } from 'react'
 import { useTheme } from '@common/providers/ThemeProvider'
-import Box, { BoxProps } from './index'
+import Box, {
+  BoxProps,
+  Flex as FlexComponent,
+  FlexProps,
+  Grid as GridComponent,
+  GridProps,
+  GridItem as GridItemComponent,
+  GridItemProps,
+  Block as BlockComponent,
+  BlockProps
+} from './index'
+
 function ThemedBox(props: BoxProps, ref?: Ref<any>) {
   const { mode } = useTheme()
 
   return <Box themeMode={mode} {...props} ref={ref} />
 }
 export default forwardRef(ThemedBox)
+
+function ThemedFlex(props: FlexProps, ref?: Ref<any>) {
+  const { mode } = useTheme()
+
+  return <FlexComponent themeMode={mode} {...props} ref={ref} />
+}
+export const Flex = forwardRef(ThemedFlex)
+
+function ThemedGrid(props: GridProps, ref?: Ref<any>) {
+  const { mode } = useTheme()
+  return <GridComponent themeMode={mode} {...props} ref={ref} />
+}
+export const Grid = forwardRef(ThemedGrid)
+
+function ThemedGridItem(props: GridItemProps, ref?: Ref<any>) {
+  const { mode } = useTheme()
+  return <GridItemComponent themeMode={mode} {...props} ref={ref} />
+}
+export const GridItem = forwardRef(ThemedGridItem)
+
+function ThemedBlock(props: BlockProps, ref?: Ref<any>) {
+  const { mode } = useTheme()
+  return <BlockComponent themeMode={mode} {...props} ref={ref} />
+}
+export const Block = forwardRef(ThemedBlock)
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,23 @@ import Text from '@components/atoms/Text/Themed'
 import Head from '@common/providers/Head'
 import ModeSwitch from '@components/atoms/ModeSwitch/Themed'
 import { useBreakpoints } from '@hooks'
-import Box from '@components/atoms/Box'
+import Box from '@components/atoms/Box/Themed'
 import { theme } from '@common/utils'
 import Link from '@components/atoms/Link/Themed'
+import Breadcrumbs from '@components/molecules/Breadcrumbs/Themed'
+import { Card, CardContent, CardMedia } from '@components/atoms/Card/Themed'
 
 const Home: NextPage = () => {
   const breakpoint = useBreakpoints()
   const [readMore, setReadMore] = useState(false)
   const toggleReadMore = () => setReadMore(!readMore)
-
+  const breadcrumbsLinks = [
+    { label: 'Home', href: '/' },
+    { label: 'Components', href: '#components' },
+    { label: 'Atoms', href: '#atoms' },
+    { label: 'Box', href: '#box' },
+    { label: ' Layouts with Box' }
+  ]
   return (
     <>
       <Head />
@@ -24,13 +32,12 @@ const Home: NextPage = () => {
         alignItems="center"
         as="main"
         cs={{
-          selector: '.box',
           css: {
             px: theme.space['4'],
             pt: theme.space['2'],
             pb: theme.space['8'],
             minH: theme.size['screen-h'],
-            maxW: theme.size['container-lg'],
+            maxW: theme.size['container-xl'],
             mx: 'auto'
           }
         }}
@@ -40,7 +47,6 @@ const Home: NextPage = () => {
           darkColorSchema="orange"
           variant="ghost"
           cs={{
-            selector: '.btn',
             css: {
               alignSelf: 'flex-end'
             }
@@ -48,17 +54,42 @@ const Home: NextPage = () => {
           size={breakpoint.up('sm') ? 'md' : 'sm'}
           data-testid="theme switch"
         />
+
+        <Breadcrumbs
+          maxItems={2}
+          color="gray"
+          cs={{
+            css: {
+              alignSelf: 'flex-start',
+              mb: theme.space['4']
+            }
+          }}
+        >
+          {breadcrumbsLinks.map((link, index) =>
+            link.href ? (
+              <Link href={link.href} key={index} fontSize="sm">
+                {link.label}
+              </Link>
+            ) : (
+              <Text fontSize="sm" as="p" key={index}>
+                {link.label}
+              </Text>
+            )
+          )}
+        </Breadcrumbs>
         <Text
           fontSize={
             breakpoint.up('xl') ? '3xl' : breakpoint.up('sm') ? 'xl' : 'lg'
           }
           as="h1"
+          fontWeight="semibold"
+          cs={{
+            css: {
+              mb: theme.space['2']
+            }
+          }}
         >
-          Click to switch theme
-        </Text>
-        <Text fontSize={breakpoint.up('sm') ? 'md' : 'sm'}>
-          Layout made with {'<Box  display="grid"/>'} and{' '}
-          {'<Box  display="grid-item"/>'}
+          Layouts with Box
         </Text>
         <Box
           display="grid"
@@ -69,10 +100,9 @@ const Home: NextPage = () => {
           templateColumns={'150px 1fr'}
           gap="2"
           cs={{
-            selector: '.box',
             css: {
-              mt: theme.space['4'],
-              maxW: theme.size['container-md'],
+              my: theme.space['8'],
+              maxW: theme.size['container-sm'],
               w: theme.size.full,
               minH: theme.size['72'],
               fontWeight: theme.fontWeight.bold
@@ -83,7 +113,6 @@ const Home: NextPage = () => {
             area={'header'}
             display="grid-item"
             cs={{
-              selector: '.box',
               css: {
                 pl: theme.space['2'],
                 bgColor: theme.paletteColor['orange-300']
@@ -96,7 +125,6 @@ const Home: NextPage = () => {
             area={'nav'}
             display="grid-item"
             cs={{
-              selector: '.box',
               css: {
                 pl: theme.space['2'],
                 bgColor: theme.paletteColor['pink-300']
@@ -109,7 +137,6 @@ const Home: NextPage = () => {
             area={'main'}
             display="grid-item"
             cs={{
-              selector: '.box',
               css: {
                 pl: theme.space['2'],
                 bgColor: theme.paletteColor['green-300']
@@ -122,7 +149,6 @@ const Home: NextPage = () => {
             area={'footer'}
             display="grid-item"
             cs={{
-              selector: '.box',
               css: {
                 pl: theme.space['2'],
                 bgColor: theme.paletteColor['blue-300']
@@ -132,56 +158,108 @@ const Home: NextPage = () => {
             Footer
           </Box>
         </Box>
-        <Text
-          noOfLines={readMore ? undefined : breakpoint.up('md') ? 5 : 15}
-          fontSize={breakpoint.up('sm') ? 'md' : 'sm'}
+        <Card
           cs={{
-            selector: '.text',
             css: {
-              maxW: theme.size['container-lg'],
-              mt: theme.space['8'],
-              mb: theme.space['8']
+              my: theme.space['8'],
+              maxW: {
+                xs: theme.size['container-sm'],
+                md: theme.size['container-lg']
+              }
             }
           }}
+          size={breakpoint.up('lg') ? 'md' : 'sm'}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu enim a
-          eros eleifend dignissim in pretium leo. Etiam quis lorem ante.
-          Maecenas a risus laoreet, porta tortor vel, ultrices ante. Duis
-          suscipit sit amet diam eget mattis. Aenean lobortis lacinia mi,
-          sagittis pulvinar felis ornare ut. Duis tempus ex eleifend sem
-          dignissim porta. Aliquam id ornare justo. Nulla posuere ante at quam
-          aliquet, in faucibus leo pretium. Vivamus eget maximus purus, non
-          finibus erat. Interdum et malesuada fames ac ante ipsum primis in
-          faucibus. Cras lectus tellus, scelerisque at quam vitae, vestibulum
-          eleifend metus. Donec at facilisis ligula, non gravida odio. Proin nec
-          orci urna. Suspendisse fermentum, ligula a venenatis vestibulum, massa
-          lacus iaculis enim, eget consectetur tellus augue vitae arcu. Proin
-          eros libero, pharetra vitae metus et, convallis pharetra lacus.
-          Vivamus facilisis sed ipsum vel egestas. Aenean aliquet justo vel dui
-          malesuada commodo. Donec volutpat leo ex, porttitor fermentum ex
-          rutrum ac. Mauris eu eros ut urna pharetra vehicula sed eu elit.
-          Vivamus varius tempor dignissim. Phasellus pellentesque velit rhoncus
-          vulputate aliquet. Vestibulum nec leo nisi. Vestibulum facilisis,
-          mauris a luctus convallis, lacus lectus consectetur metus, tincidunt
-          pulvinar lorem leo sit amet libero. Vivamus eleifend, elit sit amet
-          dapibus molestie, ipsum turpis euismod justo, eget interdum dolor
-          dolor sit amet nulla. Aliquam consectetur risus at augue lacinia
-          suscipit. Vestibulum id fermentum sapien{' '}
-          <Link
-            color="teal"
-            href="https://github.com/BrisaDiaz/next-template"
-            isExternal={true}
+          <CardContent
+            alignItems="flex-start"
+            gap="4"
+            flexDirection={breakpoint.up('md') ? 'row' : 'column'}
+            rounded={true}
           >
-            Check the documentation
-          </Link>
-        </Text>
-        <Button
-          size={breakpoint.up('md') ? 'md' : 'sm'}
-          onClick={() => toggleReadMore()}
-          colorSchema={breakpoint.up('md') ? 'blue' : 'red'}
-        >
-          {readMore ? 'Read Less' : 'Read More'}
-        </Button>
+            <CardMedia
+              height={512}
+              width={512}
+              rounded={true}
+              src="/images/ico/maskable_icon_x512.png"
+              unoptimized
+              loader={({ src }) => src}
+              alt="og image"
+              layout="responsive"
+              cs={{
+                css: {
+                  w: { xs: '100%', md: '30%' },
+                  h: 'auto',
+                  mx: 'auto'
+                }
+              }}
+            />
+            <CardContent
+              flexDirection="column"
+              alignItems={breakpoint.up('md') ? 'flex-start' : 'center'}
+              cs={{ css: { w: { xs: '100%', md: '70%' } } }}
+              gap="4"
+              bordered={breakpoint.up('md') ? 'left' : 'none'}
+            >
+              <Text
+                fontWeight="semibold"
+                as="h2"
+                fontSize={
+                  breakpoint.up('lg')
+                    ? '2xl'
+                    : breakpoint.up('sm')
+                    ? 'xl'
+                    : 'lg'
+                }
+              >
+                Duis suscipit
+              </Text>
+              <Text
+                noOfLines={readMore ? undefined : breakpoint.up('md') ? 4 : 10}
+                fontSize={breakpoint.up('lg') ? 'md' : 'sm'}
+                cs={{
+                  css: {
+                    h: 'auto'
+                  }
+                }}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu
+                enim a eros eleifend dignissim in pretium leo. Etiam quis lorem
+                ante. Maecenas a risus laoreet, porta tortor vel, ultrices ante.
+                Duis suscipit sit amet diam eget mattis. Aenean lobortis lacinia
+                mi, sagittis pulvinar felis ornare ut. Duis tempus ex eleifend
+                sem dignissim porta. Aliquam id ornare justo. Nulla posuere ante
+                at quam aliquet, in faucibus leo pretium. Vivamus eget maximus
+                purus, non finibus erat. Interdum et malesuada fames ac ante
+                ipsum primis in faucibus. Cras lectus tellus, scelerisque at
+                quam vitae, vestibulum eleifend metus. Donec at facilisis
+                ligula, non gravida odio. Proin nec orci urna. Suspendisse
+                fermentum, ligula a venenatis vestibulum, massa lacus iaculis
+                enim, eget consectetur tellus augue vitae arcu. Proin eros
+                libero, pharetra vitae metus et, convallis pharetra lacus.
+                Vivamus facilisis sed ipsum vel egestas. Aenean aliquet justo
+                vel dui malesuada commodo. Donec volutpat leo ex, porttitor
+                fermentum ex rutrum ac. Mauris eu eros ut urna pharetra vehicula
+                sed eu elit. Vivamus varius tempor dignissim.
+                <Link
+                  color="teal"
+                  fontSize={breakpoint.up('lg') ? 'md' : 'sm'}
+                  href="https://github.com/BrisaDiaz/next-template"
+                  isExternal={true}
+                >
+                  Check the documentation
+                </Link>
+              </Text>
+              <Button
+                variant="outline"
+                size={breakpoint.up('md') ? 'md' : 'sm'}
+                onClick={() => toggleReadMore()}
+                colorSchema="teal"
+              >
+                {readMore ? 'Read Less' : 'Read More'}
+              </Button>
+            </CardContent>
+          </CardContent>
+        </Card>
       </Box>
     </>
   )
