b1f9a8cdf086326648116f6a2844697080cb7abe
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-11-28 21:59:21 -0300

    feat: add  ButtonGroup component

--- components/atoms/Button/Button.test.tsx
+++ components/atoms/Button/Button.test.tsx
@@ -4,9 +4,11 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import { composeStories } from '@storybook/testing-react'
 import { Moon } from '../SVG'
 // import Button stories file as a module
-import * as stories from './Button.stories'
-const { Default, Disable, ExtraSmall } = composeStories(stories)
-
+import * as buttonStories from './Button.stories'
+import * as buttonGroupStories from './ButtonGroup.stories'
+const { Default, Disable, ExtraSmall } = composeStories(buttonStories)
+const { Default: DefaultButtonGroup } = composeStories(buttonGroupStories)
+import { buttonGroupDefaultValues, Size, Variant, BtnColorSchema } from '.'
 test('render children', () => {
   render(<Default />)
 
@@ -120,4 +122,57 @@ test('set  data-theme="light" when  prop themeMode="light"', () => {
   const buttonElement = screen.getByRole('button')
 
   expect(buttonElement).toHaveAttribute('data-theme', 'light')
+})
+export const buttonsData = [
+  { text: 'button 1', colorSchema: 'green' },
+  { text: 'button 2', colorSchema: 'blue' },
+  { text: 'button 3', colorSchema: 'red' },
+  { text: 'button 4', colorSchema: 'yellow' }
+]
+describe('ButtonGroup', () => {
+  test('render with default styles', () => {
+    render(<DefaultButtonGroup data-testid="button-group" />)
+    const flexDirection =
+      buttonGroupDefaultValues.orientation === 'horizontal' ? 'row' : 'column'
+    const buttonGroup = screen.getByTestId('button-group')
+    expect(buttonGroup).toHaveStyle(`flex-direction:${flexDirection}`)
+    const buttons = screen.getAllByRole('button')
+    expect(buttons.length).toBe(buttonsData.length)
+    buttons.forEach((button, index) => {
+      expect(button).toHaveTextContent(buttonsData[index].text)
+      expect(button.classList).toContain(
+        `btn-${buttonsData[index].colorSchema}-${buttonGroupDefaultValues.variant}`
+      )
+      expect(button.classList).toContain(`btn-${buttonGroupDefaultValues.size}`)
+      if (buttonGroupDefaultValues.rounded) {
+        expect(button.classList).toContain(`btn-rounded`)
+      } else {
+        expect(button.classList).not.toContain(`btn-rounded`)
+      }
+    })
+  })
+  test('render with custom styles', () => {
+    const customProps = {
+      rounded: true,
+      size: 'lg' as Size,
+      variant: 'outline' as Variant,
+      colorSchema: 'teal' as BtnColorSchema,
+      orientation: 'vertical' as const
+    }
+    render(<DefaultButtonGroup data-testid="button-group" {...customProps} />)
+    const flexDirection = 'column'
+    const buttonGroup = screen.getByTestId('button-group')
+    expect(buttonGroup).toHaveStyle(`flex-direction:${flexDirection}`)
+    const buttons = screen.getAllByRole('button')
+    expect(buttons.length).toBe(buttonsData.length)
+    buttons.forEach((button, index) => {
+      expect(button).toHaveTextContent(buttonsData[index].text)
+      expect(button.classList).toContain(
+        `btn-${customProps.colorSchema}-${customProps.variant}`
+      )
+      expect(button.classList).toContain(`btn-${customProps.size}`)
+
+      expect(button.classList).toContain(`btn-rounded`)
+    })
+  })
 })
--- components/atoms/Button/Themed.tsx
+++ components/atoms/Button/Themed.tsx
@@ -1,9 +1,19 @@
 import { forwardRef, Ref } from 'react'
 import { useTheme } from '@common/providers/ThemeProvider'
-import Button, { ButtonProps } from '.'
+import Button, {
+  ButtonProps,
+  ButtonGroup as ButtonGroupComponent,
+  ButtonGroupProps
+} from '.'
 function ThemedButton(props: ButtonProps, ref?: Ref<HTMLButtonElement>) {
   const { mode } = useTheme()
 
   return <Button themeMode={mode} {...props} ref={ref} />
 }
 export default forwardRef(ThemedButton)
+function ThemedButtonGroup(props: ButtonGroupProps, ref?: Ref<unknown>) {
+  const { mode } = useTheme()
+
+  return <ButtonGroupComponent themeMode={mode} {...props} ref={ref} />
+}
+export const ButtonGroup = forwardRef(ThemedButtonGroup)
